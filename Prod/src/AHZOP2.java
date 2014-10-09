eec7290b0af4f350f5e465725066068906533715
Ahmed Zouari <asz130230@utdallas.edu>
2014-10-09 09:27:10 -0500

    Added splay tree Removed level attribute Added get level function

--- Prod/src/AHZOP2.java
+++ Prod/src/AHZOP2.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 import java.util.Queue;
 import java.util.Random;
 
-
 class UnderflowException extends RuntimeException {
 	public UnderflowException() {
 	}
@@ -15,7 +14,7 @@ class UnderflowException extends RuntimeException {
 class BST<AnyType extends Comparable<? super AnyType>> {
 
 	int pCnt = 0;// ProbeCounter
-	int lCnt = 0;// Level Counter
+	int rCnt = 0;// Rotation Counter
 
 	/**
 	 * Construct the tree.
@@ -31,7 +30,7 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 	 *            the item to insert.
 	 */
 	public void insert(AnyType x) {
-		root = insert(x, root,1);
+		root = insert(x, root);
 	}
 
 	/**
@@ -121,13 +120,98 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 		printTreeBF(root);
 	}
 
-	private TreeNode splay(AnyType x, TreeNode<AnyType> t) {
-		if (t == null) {
-			return null;
-		}
-		int compareResult = x.compareTo(t.element);
-		return t;
+	/**
+	 * Internal method to get the level of a node. This function will leverage
+	 * the BFS printing and splaying of a node
+	 * 
+	 * @param k
+	 *            Any Comparable type
+	 * @return the level of the tree
+	 */
 
+	private int getLevel(AnyType k) {
+		int j = 1;
+		TreeNode<AnyType> tmpNode = root;
+		try {
+			while (k.compareTo(tmpNode.element) != 0) {
+				if (tmpNode.element.compareTo(k) > 0) {
+					tmpNode = tmpNode.left;
+				} else
+					tmpNode = tmpNode.right;
+				j++;
+
+			}
+		} catch (NullPointerException e) {
+			return -1;
+		}
+		return j;
+	}
+
+	public void splay(AnyType x) {
+		root = splay(x, root);
+	}
+
+	private TreeNode<AnyType> splay(AnyType x, TreeNode<AnyType> t) {
+		if (t == null)
+			return null;
+
+		// Left side of the tree
+		if (x.compareTo(t.element) < 0) {
+			if (t.left == null) {
+				return t;
+			}
+			if (x.compareTo(t.left.element) < 0) {
+				if (t == root & ((getLevel(x) % 2) == 0)) {
+					// zig
+					t.left = splay(x, t.left);
+					t = rotateWithLeftChild(t);
+					rCnt++;
+				} else {
+					// zig zig
+					t.left.left = splay(x, t.left.left);
+					t = rotateWithLeftChild(t);
+					t = rotateWithLeftChild(t);
+					rCnt++;
+
+				}
+			} else if (x.compareTo(t.left.element) > 0) {
+				// Zig Zag
+				t.left.right = splay(x, t.left.right);
+				t = doubleWithLeftChild(t);
+				rCnt++;
+			} else
+				return t;
+
+			// Right side
+		} else if (x.compareTo(t.element) > 0) {
+			if (t.right == null) {
+				return t;
+			}
+			if (x.compareTo(t.right.element) > 0) {
+				if (t == root & ((getLevel(x) % 2) == 0)) {
+					// zig
+					t.right = splay(x, t.right);
+					t = rotateWithRightChild(t);
+					rCnt++;
+				} else {
+					// zig zig
+					t.right.right = splay(x, t.right.right);
+					t = rotateWithRightChild(t);
+					t = rotateWithRightChild(t);
+					rCnt++;
+
+				}
+			} else if (x.compareTo(t.right.element) < 0) {
+				// Zig Zag
+				t.right.left = splay(x, t.right.left);
+				t = doubleWithRightChild(t);
+				rCnt++;
+			} else
+				return t;
+
+		} else
+			return t;
+		return t;
 	}
 
 	/**
@@ -152,10 +236,11 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 	 * @return
 	 */
 
-	private TreeNode rotateLeft(TreeNode<AnyType> t) {
+	private TreeNode<AnyType> rotateWithLeftChild(TreeNode<AnyType> t) {
 		TreeNode<AnyType> tmp = t.left;
 		t.left = tmp.right;
 		tmp.right = t;
+		// rCnt++;
 
 		return tmp;
 	}
@@ -166,12 +251,23 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 	 * @return
 	 */
 
-	private TreeNode rotateRight(TreeNode<AnyType> t) {
+	private TreeNode<AnyType> rotateWithRightChild(TreeNode<AnyType> t) {
 		TreeNode<AnyType> tmp = t.right;
 		t.right = tmp.left;
 		tmp.left = t;
+		// rCnt++;
 
 		return tmp;
+	}
+
+	private TreeNode<AnyType> doubleWithLeftChild(TreeNode<AnyType> t) {
+		t.left = rotateWithRightChild(t.left);
+		return rotateWithLeftChild(t);
+	}
+
+	private TreeNode<AnyType> doubleWithRightChild(TreeNode<AnyType> t) {
+		t.right = rotateWithLeftChild(t.right);
+		return rotateWithRightChild(t);
 	}
 
 	/**
@@ -183,24 +279,24 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 	 *            the node that roots the subtree.
 	 * @return the new root of the subtree.
 	 */
-	private TreeNode<AnyType> insert(AnyType x, TreeNode<AnyType> t , Integer level) {
+	private TreeNode<AnyType> insert(AnyType x, TreeNode<AnyType> t) {
 		if (t == null)
-			return new TreeNode<>(x, null, null , level);
+			return new TreeNode<>(x, null, null);
 
 		int compareResult = x.compareTo(t.element);
 
 		if (compareResult < 0) {
-			level++; //Increment the level
-			t.left = insert(x, t.left, level);
-			
+
+			t.left = insert(x, t.left);
+
 		}
-			
+
 		else if (compareResult > 0) {
-			level++;//Increment the level
-			t.right = insert(x, t.right, level);
-			
+
+			t.right = insert(x, t.right);
+
 		}
-			
+
 		else
 			; // Duplicate; do nothing
 		return t;
@@ -282,11 +378,13 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 
 		if (compareResult < 0) {
 			// increase the number of the probes
-			pCnt++;
+			if (t.left != null)
+				pCnt++;
 			return contains(x, t.left);
 		} else if (compareResult > 0) {
 			// increase the number of the probes
-			pCnt++;
+			if (t.right != null)
+				pCnt++;
 			return contains(x, t.right);
 		} else
 			return true; // Match
@@ -306,7 +404,6 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 		}
 	}
 
-	
 	/**
 	 * Internal method that print the tree in BFS order.
 	 * 
@@ -326,19 +423,15 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 			TreeNode<AnyType> current = que.poll();
 
 			if (current != null) {
-				currLevel = current.level;
-
-				
+				currLevel = getLevel(current.element);
 
 				if (currLevel == prevLevel)
-					System.out.print(current.element + " "  );
+					System.out.print(current.element + " ");
 				if (currLevel != prevLevel) {
 					System.out.println();
-					System.out.print(current.element + " "  );
-					
-				} 
-					
-			
+					System.out.print(current.element + " ");
+
+				}
 
 				que.add(current.left);
 				que.add(current.right);
@@ -367,18 +460,18 @@ class BST<AnyType extends Comparable<? super AnyType>> {
 	private static class TreeNode<AnyType> {
 		// Constructors
 		TreeNode(AnyType theElement) {
-			this(theElement, null, null , null);
+			this(theElement, null, null);
 		}
 
-		TreeNode(AnyType theElement, TreeNode<AnyType> lt, TreeNode<AnyType> rt ,  Integer lv) {
+		TreeNode(AnyType theElement, TreeNode<AnyType> lt, TreeNode<AnyType> rt) {
 			element = theElement;
 			left = lt;
 			right = rt;
-			level = lv;
+
 		}
 
 		AnyType element; // The data in the node
-		Integer level;
+
 		TreeNode<AnyType> left; // Left child
 		TreeNode<AnyType> right; // Right child
 	}
@@ -402,6 +495,7 @@ public class AHZOP2 {
 		int fCnt = 0; // Find Counter;
 		int tProbCnt = 0;// Total Prob Counter;
 		float aProbes = 0; // Average number of probes.
+		float aSplays = 0; // Average number of rotations during splays
 		int sCnt = 0; // Total number of splays
 		int[] p = new int[500];
 
@@ -443,12 +537,14 @@ public class AHZOP2 {
 					System.out.println("Key " + inKey + " not found");
 					tProbCnt += T.pCnt;
 					fCnt++;
+
 					break;
 				}
 
 				tProbCnt += T.pCnt;
 				fCnt++;
 				System.out.println("Key " + inKey + " found");
+
 				break;
 
 			case 'B':
@@ -461,6 +557,7 @@ public class AHZOP2 {
 				}
 
 			case 'E':
+				int rotCount = T.rCnt;
 				T = null;
 				System.out.println("The toltal number of inserts = " + iCnt);
 				System.out.println("The toltal number of deletes = " + dCnt);
@@ -468,46 +565,68 @@ public class AHZOP2 {
 				System.out
 						.println("The toltal number of nodes probed during searches = "
 								+ tProbCnt);
-				aProbes = (float) tProbCnt / fCnt;
+				if (fCnt > 0) {
+					aProbes = (float) tProbCnt / fCnt;
+				} else
+					aProbes = 0;
 				System.out
 						.print("The average number of nodes probed during searches = ");
 				System.out.printf("%.2f", aProbes);
 				System.out.println();
+				System.out
+						.println("The total number of rotations during splays = "
+								+ rotCount);
+				aSplays = (float) rotCount / sCnt;
+				System.out
+						.print("The average number of rotations during splays = ");
+				System.out.printf("%.2f", aSplays);
+				System.out.println();
+
 				break;
 
 			case 'Z':
 				System.out.println(T.size());
 				break;
 
+			case 'S':
+				inKey = reader.nextInt();
+				if (!T.contains(inKey)) {
+					System.out.println("Key " + inKey + " not found");
+					sCnt++;
+					break;
+				}
+				T.splay(inKey);
+				System.out.println("key " + inKey + " splayed");
+				sCnt++;
+				break;
+
 			case 'R':
 				T.makeEmpty();
-				
+
 				Random rand = new Random();
-				//Create an array
+				// Create an array
 				int i = 0;
 				for (i = 0; i < 500; i++) {
 					p[i] = i + 1;
 				}
-				//Shuffle the array
-				for (i = 0 ; i < 500 ; i++) {
+				// Shuffle the array
+				for (i = 0; i < 500; i++) {
 					int b = rand.nextInt(500);
 					int a = p[i];
-					
+
 					p[i] = p[b];
 					p[b] = a;
-					
-					
-					
+
 				}
-				for (i = 0; i < 50000; i++) {
+				for (i = 0; i < 5000; i++) {
 					inKey = p[rand.nextInt(500)];
-					if (T.contains(inKey)) {
-						// Do Nothing
-					} else {
-						T.insert(inKey);
-						iCnt++;
-					}
+					
+					T.insert(inKey);
+					T.splay(inKey);
+					sCnt++;
+					iCnt++;
 				}
+				// }
 				break;
 
 			default:
