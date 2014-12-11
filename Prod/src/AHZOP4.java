931abb424aaedf9f486abc3750bdfdee6571cb97
Ahmed Zouari <asz130230@utdallas.edu>
2014-12-10 18:41:46 -0600

    new

--- Prod/src/AHZOP4.java
+++ Prod/src/AHZOP4.java
@@ -1,135 +1,99 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * 
  * @author ahmedzouari
  *
  */
-import java.util.ArrayList;
-import java.util.Queue;
-import java.util.Scanner;
-import java.util.Stack;
-import java.util.PriorityQueue;
-
 /**
- * Class that holds the edges
+ * Graph Edge class
  * @author ahmedzouari
  *
  */
-class DirectedEdge {
-	private final int v, w;
-	private final int weight;
+class GraphEdge {
+	final int i; //Vertex 1
+	final int j; //Vertex 2
+	final int w; //Weight
 	
-	/**
-	 * Class Constructor
-	 * @param v
-	 * @param w
-	 * @param weight
-	 */
-	public DirectedEdge( int v , int w , int weight ) {
-		this.v = v;
+	GraphEdge (int i, int j , int w) {
+		this.i = i;
+		this.j = j;
 		this.w = w;
-		this.weight = weight;
 	}
 	
-	public int from () {
-		return v;
-	}
-	
-	public int to () {
-		return w;
-	}
-	
-	public int weight() {
-		return weight;
+	public String toString() {
+		return i + " " + j + " " + w;
 	}
 	
 }
 
-class DirectGraph {
-	final int V;//Number of vertices
-	int E;//Number of Edges
-	ArrayList<DirectedEdge> []  adj ; //Array of ArryaList
+
+/**
+ * Directed Weighted Graph class
+ * @author ahmedzouari
+ *
+ */
+class DWG {
+	int V;//Number of vertices
 	
-	DirectGraph (int V) {
+	ArrayList<GraphEdge> ALS[];
+	
+	DWG (int V) {
 		this.V = V;
-		this.E = 0;
-		adj = new ArrayList[V];
+		ALS = new ArrayList[V];
 		for (int v = 0 ; v < V; v++) {
-			adj[v] = new ArrayList<DirectedEdge>();
+			ALS[v] = new ArrayList<GraphEdge>();
 		}
+		
 	}
 	
-	void addEdge ( int v , int w , int weight) {
-		DirectedEdge e = new DirectedEdge(v,w,weight);
-		adj[v].add(e);
+	void addEdge(int i , int j , int w) {
+		GraphEdge e = new GraphEdge(i,j,w);
+		ALS[i].add(e);
 	}
+	
+	public void printing() {
+		for (int i = 0 ; i < V ; i++) {
+			
+			for (GraphEdge e : ALS[i]) {
+				System.out.println(e.toString() + " ");
+			}
+			
+			
+		}
+		
+	}
+	
+	
 }
-
 public class AHZOP4 {
 
 	public static void main(String[] args) {
-		Queue<Integer>  q = new java.util.LinkedList<Integer>();
-		/*
-		DirectGraph G = new DirectGraph(10);
-		G.addEdge(0, 1, 10);
-		G.addEdge(0, 2, 10);
-		G.addEdge(0, 3, 10);
-		G.addEdge(2, 3, 10);
-		for (int i = 0 ; i < 10 ; i++) {
-		for ( DirectedEdge e : G.adj[i]) {
-			System.out.println(e.from() + " ==> " + e.to() + " weight:" + e.weight());
-		}
-		}
-		*/
-		
-	
-		
-				
-		
-		String title;
-		int nVertices; // Number of vertices
-		int sVertex; // Source vertex
-		int v;
-		int w;
-		int weight;
-		
-		
-		
+		String title;//Graph title
+		int V;//Number of vertices
+		int sVertex;//Start vertex
+		int i,j,w;//Edge i , j and weight w
 		
 		
 		Scanner reader = new Scanner(System.in);
-
 		title = reader.nextLine();
-		nVertices = reader.nextInt();
+		V = reader.nextInt();
 		sVertex = reader.nextInt();
 		
-		DirectGraph G = new DirectGraph(nVertices);
+		DWG G = new DWG(V);
 		
-		System.out.println(title);
-		System.out.print(nVertices + " ");
-		System.out.println(sVertex);
-
-		reader.nextLine();
-		while (reader.hasNextLine()) {
-			v = reader.nextInt();
-			w = reader.nextInt();
-			weight = reader.nextInt();
-			
-			G.addEdge(v, w, weight);
-			
-			
-			for (int i = 0 ; i < nVertices ; i++) {
-				for ( DirectedEdge e : G.adj[i]) {
-					System.out.println(e.from() + "->" + e.to() + " " + e.weight());
-				}
-				}
-
-			reader.nextLine();
-
-		}
-
-		reader.close();
+		G.addEdge(1, 2, 5);
+		G.addEdge(1, 3, 5);
+		G.addEdge(1, 4, 5);
+		G.addEdge(2, 4, 5);
 		
-
+		G.printing();
+		
+		
+		
+		System.out.println(title + " " + V + " " + sVertex );
+		
 
 	}
 
